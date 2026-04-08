85350ad04b028ebf30098ff654e5c8b5c6029dec
dobbyncicode <gphreak04@gmail.com>
2026-04-09 03:21:50 +0800

    fix: update abbr

--- home/dotfiles/caelestia/user-config.fish
+++ home/dotfiles/caelestia/user-config.fish
@@ -6,6 +6,7 @@ abbr dr "sudo dotdrop install --cfg={{@@ _dotdrop_cfgpath @@}}/config-root.yaml"
 abbr gac --set-cursor "git add . && git commit -m \"%\""
 abbr upo "yay -Syyu --noconfirm && poweroff"
 abbr upr "yay -Syyu --noconfirm && reboot"
+abbr gp "git push -u origin main"
 
 # --- [ i ] - Environment variables section ---
 set -gx EDITOR {{@@ editor @@}}
