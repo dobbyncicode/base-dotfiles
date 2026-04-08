dcdc3ae252a419a55955150858756b68070256c8
dobbyncicode <gphreak04@gmail.com>
2026-04-09 03:20:38 +0800

    fix: no hardcoded repo dir

--- home/dotfiles/caelestia/user-config.fish
+++ home/dotfiles/caelestia/user-config.fish
@@ -1,8 +1,8 @@
 mise activate fish | source
 
 # --- [ i ] - Abbr section ---
-abbr di "dotdrop install --cfg=/home/dobby/aa/config.yaml"
-abbr dr "sudo dotdrop install --cfg=/home/dobby/aa/config-root.yaml"
+abbr di "dotdrop install --cfg={{@@ _dotdrop_cfgpath @@}}/config.yaml"
+abbr dr "sudo dotdrop install --cfg={{@@ _dotdrop_cfgpath @@}}/config-root.yaml"
 abbr gac --set-cursor "git add . && git commit -m \"%\""
 abbr upo "yay -Syyu --noconfirm && poweroff"
 abbr upr "yay -Syyu --noconfirm && reboot"
